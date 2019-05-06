d43942b07b0bcd7a2323a220b8876fb0b9704462
Sofia-Pika <delijajsofia@yahoo.com>
2019-05-06 12:29:25 +0200

    formating the date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,7 +7,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Developers market app';
   constructor() { }
+  version = VERSION.full;
 
+  formatsDateTest: string[] = [
+    'dd/MM/yyyy',
+    'dd/MM/yyyy hh:mm:ss',
+    'dd-MM-yyyy',
+    'dd-MM-yyyy HH:mm:ss',
+    'MM/dd/yyyy',
+    'MM/dd/yyyy hh:mm:ss',
+    'yyyy/MM/dd',
+    'yyyy/MM/dd HH:mm:ss',
+    'dd/MM/yy',
+    'dd/MM/yy hh:mm:ss',
+  ];
+
+  dateNow: Date = new Date();
+  dateNowISO = this.dateNow.toISOString();
+  dateNowMilliseconds = this.dateNow.getTime();
   ngOnInit() {
 
   }
