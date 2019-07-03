fc6610cb361652d0cf65af3ae006e4161c92caac
Sofia-Pika <delijajsofia@yahoo.com>
2019-07-03 16:20:29 +0200

    profil update

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from "../interfaces/user";
 import { AuthService } from '../auth/auth.service';
+import { ApiTransactionService } from '../services/api/api-transaction.service';
+import { Transaction } from 'src/app/interfaces/transaction';
+import { Contribution } from '../interfaces/contribution';
 
 @Component({
   selector: 'app-profil',
@@ -9,6 +12,8 @@ import { AuthService } from '../auth/auth.service';
 })
 export class ProfilComponent implements OnInit {
   public currentUser: User;
+  public allTransaction: Transaction;
+  public allContribution: Contribution;
 
   constructor(private authService: AuthService) { }
 
@@ -16,7 +21,9 @@ export class ProfilComponent implements OnInit {
     this.currentUser = localStorage.getItem("CURRENT_USER") ? JSON.parse(localStorage.getItem("CURRENT_USER")) : null;
     this.authService.authenticationEvent.subscribe(user => {
       this.currentUser = user;
-    });
+    })
+      ;
+
   }
 
 }
